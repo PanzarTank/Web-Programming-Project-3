1349db41b47aeb7c5a0f098a7669dd8c9b77a648
sungjkim34 <sungjkim34@gmail.com>
2018-04-11 14:02:22 -0400

    add item

--- src/models/Item.java
+++ src/models/Item.java
@@ -14,6 +14,12 @@ public class Item {
         this.quantity = quantity;
     }
 
+    public Item(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
     public int getItemId() {
         return id;
     }
