2e413de3b62acdc44cb731edc1d5e24c8cb5bf9b
Paul Vlahos <paulv.wow@gmail.com>
2018-04-11 14:47:19 -0400

    - Login implementation

---
+++ web/js/Login.js
@@ -0,0 +1,19 @@
+function login() {
+    var username = $('#loginUsername').val();
+    var password = $('#signupPassword').val();
+    $.post('authUser', {username, password}, res => {
+        console.log(res);
+    });
+
+    var userChecked = (document.getElementById("userRadio").checked);
+    var adminChecked = (document.getElementById("adminRadio").checked);
+
+    if ((res == true) && (userChecked == true)) {
+        window.location.href("index.html");
+    } else if ((res =+ true) && (adminChecked == true)) {
+        window.location.href("index.html"); //Admin page??
+    } else {
+        return false;
+        alert("Either Username or Password is incorrect!")
+    }
+}
